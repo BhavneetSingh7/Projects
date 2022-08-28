8face79fd5f556da452e9369562725b82ef9a93d
Bhavneet Singh <97750271+BhavneetSingh7@users.noreply.github.com>
2022-08-28 21:36:19 +0530

    Update number.py

--- regex/numbers/number.py
+++ regex/numbers/number.py
@@ -1,1 +1,12 @@
+import re
 
+pattern = re.compile(r'[\+]91\s\d{10}')
+
+with open('random_numbers.txt','r',encoding='utf-8') as f:
+    contents = f.read()
+    matches = pattern.finditer(contents)
+    count = 0
+    for match in matches:
+        count += 1
+        #print(match)
+    print(count)
